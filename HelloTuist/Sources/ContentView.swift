189a06fe14e5ecb5821b508d155662fdd3c7da91
Javier Calatrava <javi.calatrava@gmail.com>
2025-05-31 17:53:53 +0200

    Presented version

--- HelloTuist/Sources/ContentView.swift
+++ HelloTuist/Sources/ContentView.swift
@@ -1,11 +1,27 @@
 import SwiftUI
 
-public struct ContentView: View {
-    public init() {}
-
-    public var body: some View {
-        Text("Hello, World!")
-            .padding()
+struct ContentView: View {
+    var appVersion: String {
+        let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "?"
+        let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? "?"
+        return "Version \(version) (\(build))"
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("Information")) {
+                    HStack {
+                        Label("App versusion", systemImage: "number")
+                        Spacer()
+                        Text(appVersion)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationTitle("Hello Tuist!")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
--- Project.swift
+++ Project.swift
@@ -1,24 +1,25 @@
 import ProjectDescription
 
+
+
 let project = Project(
     name: "HelloTuist",
+    settings: Project.settings,
     targets: [
         .target(
             name: "HelloTuist",
             destinations: .iOS,
             product: .app,
             bundleId: "io.tuist.HelloTuist",
-            infoPlist: .extendingDefault(
-                with: [
-                    "UILaunchScreen": [
-                        "UIColorName": "",
-                        "UIImageName": "",
-                    ],
-                ]
-            ),
+            infoPlist: .extendingDefault(with: [
+                "CFBundleDisplayName": "Tuist App",
+                "CFBundleShortVersionString": "$(MARKETING_VERSION)",
+                "CFBundleVersion": "$(CURRENT_PROJECT_VERSION)"
+            ]),
             sources: ["HelloTuist/Sources/**"],
             resources: ["HelloTuist/Resources/**"],
-            dependencies: []
+            dependencies: [],
+            settings: Target.settings
         ),
         .target(
             name: "HelloTuistTests",
@@ -32,3 +33,22 @@ let project = Project(
         ),
     ]
 )
+
+public extension Project {
+    static let settings: Settings = {
+        let baseConfiguration: SettingsDictionary = [
+            "MARKETING_VERSION": "1.2.3",
+            "CURRENT_PROJECT_VERSION": "42"
+        ]
+        let releaseConfiguration = baseConfiguration
+        return Settings.settings(base: baseConfiguration, debug: baseConfiguration, release: releaseConfiguration)
+    }()
+}
+
+public extension Target {
+    static let settings: Settings = {
+        let baseConfiguration: SettingsDictionary = [:]
+        var releaseConfig = baseConfiguration
+        return Settings.settings(base: baseConfiguration, debug: baseConfiguration, release: releaseConfig)
+    }()
+}
